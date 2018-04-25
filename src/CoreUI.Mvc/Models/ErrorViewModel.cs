f4fc8e55b9d83179229367edde2e1e3fa0bd91cd
Miguel Veloso <mvelosop@gmail.com>
2018-04-25 19:53:18 +0100

    Initial MVC Core app

---
+++ src/CoreUI.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreUI.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
